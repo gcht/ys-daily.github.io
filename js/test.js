b06abdfb7f1fab98dcf36fcd7c0d09d7e21fa36a
gcht <gcht@163.com>
2020-03-06 00:48:58 +0800

    Update test.js

--- js/test.js
+++ js/test.js
@@ -1,20 +1,20 @@
 cb.defineInner([], function () {
-									var TestExternal = {
-										//外部调用方法入口
-										doAction: function (name, viewModel) {
-											if (this[name]) {
-												this[name](viewModel);
-											}
-										},
-										//初始化整个扩展脚本
-										init: function (viewModel) {
-										  alert('这一段测试程序')
-										  
-										  viewModel.get('btn1').on('click',function(){
-										    alert('即将跳转到新增页面')
-										  })
-											
-										}
-									}
-									return TestExternal;
-								})
+	var TestExternal = {
+		//外部调用方法入口
+		doAction: function (name, viewModel) {
+			if (this[name]) {
+				this[name](viewModel);
+			}
+		},
+		//初始化整个扩展脚本
+		init: function (viewModel) {
+		  alert('这一段测试程序')
+
+		  viewModel.get('btn1').on('Click',function(){
+		    alert('即将跳转到新增页面')
+		  })
+
+		}
+	}
+	return TestExternal;
+})
