a1b047fbb2c657d03ce48b2b374b4cc317897f5c
gcht <gcht@163.com>
2020-03-06 01:11:29 +0800

    Update test.js

--- js/test.js
+++ js/test.js
@@ -8,8 +8,8 @@ cb.defineInner([], function () {
 		},
 		//初始化整个扩展脚本
 		init: function (viewModel) {
-		  alert('这一段扩展脚本'+ (new Date()).toString());
-
+		  //alert('这一段扩展脚本'+ (new Date()).toString());
+alert('这一段扩展脚本');
 		  viewModel.get('btn1').on('click',function(){
 		    alert('即将跳转到新增页面')
 		  })
