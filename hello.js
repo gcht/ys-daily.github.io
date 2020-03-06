815112665c89fa7ccac354325269f4fe31d3fee4
gcht <gcht@163.com>
2020-03-06 12:21:55 +0800

    Update hello.js

--- hello.js
+++ hello.js
@@ -1,1 +1,15 @@
-alert(123)
+cb.defineInner([], function () {
+  var TestExternal = {
+    //外部调用方法入口
+    doAction: function (name, viewModel) {
+      if (this[name]) {
+        this[name](viewModel);
+      }
+    },
+    //初始化整个扩展脚本
+    init: function (viewModel) {
+        alert('扩展脚本执行中')
+    }
+  }
+  return TestExternal;
+})
